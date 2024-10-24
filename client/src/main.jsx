9f583ad6e444a4d2bdbde576289346d5ca1d1319
it27427 <istiak.tushar.qtec@gmail.com>
2024-10-25 02:20:37 +0600

    Toaster installed and registered in main

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,8 +3,9 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 
-import App from './App.jsx';
+import App from '@/App.jsx';
 import store from '@/store/store.js';
+import { Toaster } from '@/components/ui/toaster.jsx';
 
 // Import styles
 import 'swiper/css';
@@ -16,6 +17,7 @@ createRoot(document.getElementById('root')).render(
     <Provider store={store}>
       <StrictMode>
         <App />
+        <Toaster />
       </StrictMode>
     </Provider>
   </BrowserRouter>
