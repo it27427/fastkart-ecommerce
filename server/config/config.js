4fd52bcc479584c0fe8de7c1cebd6164df35e638
it27427 <istiak.tushar.qtec@gmail.com>
2024-10-25 01:33:46 +0600

    database config updated

--- server/config/config.js
+++ server/config/config.js
@@ -6,7 +6,7 @@ const dev = {
     port: process.env.PORT || 8080,
   },
   db: {
-    url: process.env.DB_URL || 'mongodb://localhost:27017/FastKart',
+    url: process.env.DB_URL || 'mongodb://localhost:27017/',
   },
 };
 
