857a36959a421677f785dafd3ec7476183cea71e
it27427 <istiak.tushar.qtec@gmail.com>
2024-10-25 00:46:30 +0600

    frontend server port changed

--- server/app.js
+++ server/app.js
@@ -17,7 +17,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: 'http://localhost:5173/',
+    origin: 'http://localhost:3000/',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: [
       'Content-Type',
@@ -29,7 +29,7 @@ app.use(
     credentials: true,
   })
 );
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.json());
 app.use(cookieParser());
