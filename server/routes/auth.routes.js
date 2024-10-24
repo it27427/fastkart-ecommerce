e3648a9ded2af51dcb0816c27e477cd7080eb72d
it27427 <istiak.tushar.qtec@gmail.com>
2024-10-25 00:14:38 +0600

    register route created

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -1,1 +1,10 @@
 const express = require('express');
+const router = express.Router();
+
+const {
+  registerController,
+} = require('@root/controllers/auth/Auth.Controller');
+
+router.post('/register', registerController);
+
+module.exports = router;
