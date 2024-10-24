c3458268f42eee4620023221e4862992580c9625
it27427 <istiak.tushar.qtec@gmail.com>
2024-10-24 22:44:10 +0600

    auth layout updated

--- client/src/components/auth/Layout.jsx
+++ client/src/components/auth/Layout.jsx
@@ -2,7 +2,7 @@ import { Link, Outlet } from 'react-router-dom';
 
 const AuthLayout = () => {
   return (
-    <main className='flex min-h-screen w-full'>
+    <main className='flex h-screen w-full overflow-hidden'>
       <div className="hidden lg:flex items-center justify-center bg-black w-1/2 px-12">
         <div className="max-w-md space-y-6 text-center text-primary-foreground">
           <h1 className='text-4xl font-playwriteGBS font-semibold tracking-tight'>
@@ -11,7 +11,7 @@ const AuthLayout = () => {
         </div>
       </div>
 
-      <div className='flex flex-1 items-center justify-center bg-background px-4 py-12 sm:px-6 lg:px-8'>
+      <div className='flex flex-1 items-center justify-center bg-background px-4 py-12 sm:px-6 lg:px-8 lg:min-h-screen lg:overflow-y-scroll'>
         <Outlet />
       </div>
     </main>
