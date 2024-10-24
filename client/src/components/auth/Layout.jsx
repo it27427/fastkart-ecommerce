22411e20a6adf3b159720e3bc0a8b4775520a59a
it27427 <istiak.tushar.qtec@gmail.com>
2024-10-24 22:00:57 +0600

    Register Header

--- client/src/components/auth/Layout.jsx
+++ client/src/components/auth/Layout.jsx
@@ -1,12 +1,12 @@
-import { Outlet } from 'react-router-dom';
+import { Link, Outlet } from 'react-router-dom';
 
 const AuthLayout = () => {
   return (
     <main className='flex min-h-screen w-full'>
       <div className="hidden lg:flex items-center justify-center bg-black w-1/2 px-12">
         <div className="max-w-md space-y-6 text-center text-primary-foreground">
-          <h1 className='text-4xl font-oxanium font-semibold tracking-tight'>
-            <span className='font-raleway'></span>Welcome to <span className='text-primarybg'>Fast</span>Kart
+          <h1 className='text-4xl font-playwriteGBS font-semibold tracking-tight'>
+            <span className='font-raleway'>W</span>elcome to <span className='text-primarybg'>Fast</span>Kart
           </h1>
         </div>
       </div>
--- client/src/pages/auth/Register.jsx
+++ client/src/pages/auth/Register.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import { Link } from 'react-router-dom';
 import CommonForm from '@/components/common/Form';
 import { RegisterFormControls } from '@/config/index';
 
@@ -12,6 +12,13 @@ const Register = () => {
         <h2 className='text-4xl font-oxanium font-bold tracking-tight text-foreground'>
           {title}
         </h2>
+
+          <p className='mt-2 font-oxanium'>
+            Already have an account?
+            <Link className='font-bold hover:underline hover:text-primarybg font-playwriteGBS ml-2' to={'/login'}>
+              Login
+            </Link>
+          </p>
       </div>
       {/* <CommonForm formControls={RegisterFormControls} /> */}
     </div>
