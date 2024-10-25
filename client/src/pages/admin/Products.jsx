3877402ca61dc7600ccb4207c33dad8f87315731
it27427 <istiak.tushar.qtec@gmail.com>
2024-10-26 00:50:37 +0600

    Products-Header

--- client/src/pages/admin/Products.jsx
+++ client/src/pages/admin/Products.jsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import React from 'react';
+import { Plus } from 'lucide-react';
+import { Button } from '@/components/ui/button';
 
 const Products = () => {
   return (
-    <div>Products</div>
+    <>
+      <div className='mb-5 flex justify-end w-full'>
+        <Button className='gap-1'>
+          <Plus />
+          <span>Add New Product</span>
+        </Button>
+      </div>
+
+      <div className='grid gap-4 grid-cols-2'></div>
+    </>
   )
 }
 
