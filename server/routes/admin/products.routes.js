1d6d409dba71f5ff97504239889d5b20b81502af
it27427 <istiak.tushar.qtec@gmail.com>
2024-10-27 19:39:07 +0600

    products route configuration done

--- server/routes/admin/products.routes.js
+++ server/routes/admin/products.routes.js
@@ -11,5 +11,9 @@ const {
 const { upload } = require('../../helpers/cloudinary');
 
 router.post('/upload-image', upload.single('my_file'), handleImageUpload);
+router.post('/create', addProduct);
+router.put('/update/:id', updateProduct);
+router.delete('/delete/:id', deleteProduct);
+router.get('/read', fetchAllProducts);
 
 module.exports = router;
