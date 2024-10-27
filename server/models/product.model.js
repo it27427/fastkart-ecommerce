e35348915149234587054b8b25dbd6b5686d0e33
it27427 <istiak.tushar.qtec@gmail.com>
2024-10-27 13:26:48 +0600

    product model done

--- server/models/product.model.js
+++ server/models/product.model.js
@@ -1,17 +1,20 @@
 const mongoose = require('mongoose');
 
-const ProductSchema = new mongoose.Schema({
-  image: String,
-  title: String,
-  description: String,
-  category: String,
-  brand: String,
-  size: String,
-  color: String,
-  price: Number,
-  salePrice: Number,
-  totalStock: Number,
-});
+const ProductSchema = new mongoose.Schema(
+  {
+    image: String,
+    title: String,
+    description: String,
+    category: String,
+    brand: String,
+    size: String,
+    color: String,
+    price: Number,
+    salePrice: Number,
+    totalStock: Number,
+  },
+  { timestamps: true }
+);
 
 const Product = mongoose.model('Products', ProductSchema);
 
