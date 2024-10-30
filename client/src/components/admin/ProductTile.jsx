86a1d757b526a2fa63c4404460f9d0824379c188
it27427 <istiak.tushar.qtec@gmail.com>
2024-10-30 11:38:20 +0600

    Product-Card

--- client/src/components/admin/ProductTile.jsx
+++ client/src/components/admin/ProductTile.jsx
@@ -2,7 +2,15 @@ import React from 'react';
 import { Card } from '@/components/ui/card';
 
 const ProductTile = ({ product }) => {
-  return <Card>ProductTile</Card>;
+  return (
+    <Card className='w-full max-w-sm mx-auto'>
+      <div>
+        <figure className='positive'>
+          <img src={product?.image} alt={product?.title} />
+        </figure>
+      </div>
+    </Card>
+  );
 };
 
 export default ProductTile;
