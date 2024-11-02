c095969ef7939e849b89e72f35cf714ded16c63f
it27427 <istiak.tushar.qtec@gmail.com>
2024-11-02 14:51:44 +0600

    checkbox installed

--- client/src/components/shop/Filter.jsx
+++ client/src/components/shop/Filter.jsx
@@ -1,10 +1,28 @@
 import React from 'react';
+import { filterOptions } from '@/config';
+import { Label } from '@/components/ui/label';
 
 const FilterProduct = () => {
   return (
     <div className='bg-background rounded-lg shadow-sm'>
       <div className='p-4 border-b'>
-        <h2 className='text-lg font-semibold'>Filters</h2>
+        <h2 className='text-lg font-extrabold'>Filters</h2>
+      </div>
+
+      <div className='p-4 space-y-4'>
+        {Object.keys(filterOptions).map((keyItem) => (
+          <>
+            <div className='flex flex-col gap-2'>
+              <h3 className='capitalize text-base font-bold'>{keyItem}</h3>
+
+              <div className='grid gap-2'>
+                {filterOptions[keyItem].map((option) => (
+                  <Label key={option.id}></Label>
+                ))}
+              </div>
+            </div>
+          </>
+        ))}
       </div>
     </div>
   );
