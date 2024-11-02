daf4a2c237bbcee848ca5800c80ef5eb6f60f361
it27427 <istiak.tushar.qtec@gmail.com>
2024-11-02 14:40:52 +0600

    Filter Component Created

--- client/src/pages/shop/Shop.jsx
+++ client/src/pages/shop/Shop.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
 const Shop = () => {
-  return <div>Shop</div>;
+  return (
+    <div className='grid gap-6 grid-cols-1 sm:grid-cols-2 md:grid-cols-[300px_1fr] p-4 md:p-6'>
+      Shop
+    </div>
+  );
 };
 
 export default Shop;
