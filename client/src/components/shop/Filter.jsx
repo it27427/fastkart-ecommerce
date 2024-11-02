989e5c41e59338ca724dce659bf5ac6da2167de7
it27427 <istiak.tushar.qtec@gmail.com>
2024-11-03 01:18:34 +0600

    FilterKey

--- client/src/components/shop/Filter.jsx
+++ client/src/components/shop/Filter.jsx
@@ -11,17 +11,15 @@ const FilterProduct = () => {
 
       <div className='p-4 space-y-4'>
         {Object.keys(filterOptions).map((keyItem) => (
-          <>
-            <div className='flex flex-col gap-2'>
-              <h3 className='capitalize text-base font-bold'>{keyItem}</h3>
+          <div className='flex flex-col gap-2' key={keyItem}>
+            <h3 className='capitalize text-base font-bold'>{keyItem}</h3>
 
-              <div className='grid gap-2'>
-                {filterOptions[keyItem].map((option) => (
-                  <Label key={option.id}></Label>
-                ))}
-              </div>
+            <div className='grid gap-2'>
+              {filterOptions[keyItem].map((option) => (
+                <Label key={option.id}></Label>
+              ))}
             </div>
-          </>
+          </div>
         ))}
       </div>
     </div>
